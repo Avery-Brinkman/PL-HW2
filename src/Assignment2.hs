e9bf70ae03326e0ccf1f7caebcbafd31bba49e08
Avery-Brinkman <averybrinkman@gmail.com>
2021-11-02 17:56:53 -0400

    Merge sort

--- src/Assignment2.hs
+++ src/Assignment2.hs
@@ -8,6 +8,7 @@ instance Show Found where
   show (Match index) = "Found match at " ++ show index
   show NoMatch = "No match found!"
 findFirst :: Eq a => (a -> Bool) -> [a] -> Found
+findFirst _ _ = NoMatch
 
 ------------------------------------------------
 -- runLengthEncode
@@ -16,6 +17,7 @@ data RunLength = Span Integer Char deriving Eq
 instance Show RunLength where
   show (Span length c) = "Length: " ++ show length ++ ": " ++ show c
 runLengthEncode :: [Char] -> [RunLength]
+runLengthEncode _ = [Span 3 'b']
 
 ------------------------------------------------
 -- palindrome
@@ -32,4 +34,18 @@ palindrome (x:xs) = (x == last xs) && palindrome (init xs)
 ------------------------------------------------
 -- mergesort
 ------------------------------------------------
+merge :: (Ord a) => (a -> a -> Bool) -> [a] -> [a] -> [a]
+merge _ [] y = y
+merge _ x [] = x
+merge f x y
+    | null x = y
+    | null y = x
+    | f (head x) (head y) = (head x):merge f (tail x) y
+    | otherwise = (head y):merge f x (tail y)
+
 mergesort :: (Ord a) => (a -> a -> Bool) -> [a] -> [a]
+mergesort compareFunc nums
+    | null firstHalf || null lastHalf = firstHalf ++ lastHalf
+    | otherwise = merge compareFunc (mergesort compareFunc firstHalf) (mergesort compareFunc lastHalf)
+    where (firstHalf, lastHalf) = splitAt (div(length nums) 2) nums
+
